fb6784e24a46cc20007832d82e5b63c31cc8842d
roeiam3 <135747319+roeiam3@users.noreply.github.com>
2025-06-07 18:01:50 +0300

    Update main.py

--- DNAC-TESTING/main.py
+++ DNAC-TESTING/main.py
@@ -1,5 +1,8 @@
 import subprocess
 import sys
+from dnac_client import get_token, get_topology
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def install_packages():
     try:
@@ -11,22 +14,6 @@ def install_packages():
 install_packages()
 
 
-from dnac_client import get_token, get_topology
-import networkx as nx
-import matplotlib.pyplot as plt
-
-def build_topology_graph(topology):
-    G = nx.Graph()
-    id_to_label = {node['id']: node['label'] for node in topology['nodes']}
-
-    for link in topology['links']:
-        src = id_to_label.get(link['source'])
-        dst = id_to_label.get(link['target'])
-
-        if src and dst:
-            G.add_edge(src, dst)
-
-    return G
 
 
 def build_topology_graph(topology):
@@ -41,6 +28,29 @@ def build_topology_graph(topology):
             G.add_edge(src, dst)
 
     return G
+
+
+def build_topology_graph(topology):
+    G = nx.Graph()
+    id_to_label = {node['id']: node['label'] for node in topology['nodes']}
+
+    for link in topology['links']:
+        src = id_to_label.get(link['source'])
+        dst = id_to_label.get(link['target'])
+
+        if src and dst:
+            G.add_edge(src, dst)
+
+    return G
+
+def draw_and_save_graph(G, filename="topology.png"):
+    plt.figure(figsize=(10, 8))
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=2000, font_size=10)
+    plt.title("Simplified Physical Topology from DNAC")
+    plt.savefig(filename)
+    print(f"[+] Topology image saved as {filename}")
+
 
 def main():
     token = get_token()
